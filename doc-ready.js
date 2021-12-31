07e5f6410964734f54a96a0524f217aaa9845f9a
delvauxo <delvauxo@outlook.com>
2021-12-31 20:52:40 +0100

    loader on document ready

--- doc-ready.js
+++ doc-ready.js
@@ -1,4 +1,8 @@
 // A $( document ).ready() block.
 $( document ).ready(function() {
-    console.log( "ready!" );
+
+    window.setTimeout(function () {
+        document.querySelector('#loader').style.display = 'none'
+    }, 2500)
+
 });
